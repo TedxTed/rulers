3ef47a2b978f4547c688fb8ee8d26acca745d5b6
Ted <wodf410@gmail.com>
2023-04-01 16:56:58 +0800

    init

---
+++ lib/rulers.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "rulers/version"
+
+
+module Rulers
+  class Application
+    def call(_env)
+      `echo debug > debug.txt`
+      [200, { "Content-Type" => "text/html" },
+       ["Hello from Ruby on Rulers!"]]
+    end
+  end
+end
